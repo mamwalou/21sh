89f4ebf79ad75852c38ac55dd029b28baf9ba24d
Samuel BELINE <sbeline@e1r7p21.42.fr>
2017-05-24 21:31:52 +0200

    re

--- srcs/lexer_parser/tools_lexer.c
+++ srcs/lexer_parser/tools_lexer.c
@@ -6,42 +6,21 @@
 /*   By: sbeline <sbeline@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 21:05:10 by sbeline           #+#    #+#             */
-/*   Updated: 2017/05/24 18:57:56 by sbeline          ###   ########.fr       */
+/*   Updated: 2017/05/24 21:31:06 by sbeline          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer_parser.h"
 
-void			epur_home(char **str, char **ptr)
+void			epur_home(char **str, char **ptr_after)
 {
-	char		*ptr_after;
 	char		*sigle;
-	char		*tmp;
 	int			count;
 
 	count = 0;
 	sigle = NULL;
-	ptr_after = *ptr + 1;
-	**ptr = 0;
-	dprintf(dfd,"ptr_after(%s), ptr(%s), *str(%s)\n", ptr_after, *ptr, *str);
-	while ((ptr_after[count + 1]) != '\0' || (ptr_after[count] == ' '))
-		count++;
 	if ((sigle = search_env(g_env, "HOME=")))
-	{
-		if (!*str)
-		{
-			*str = ft_strjoin(sigle, ptr_after);
-			dprintf(dfd,"ptr_after(%s), ptr(%p)\n", ptr_after, ptr_after);
-		}
-		else
-		{
-			tmp = ft_strjoin(*str, sigle);
-			free(*str);
-			*str = ft_strjoin(tmp, ptr_after);
-			free(tmp);
-			dprintf(dfd,"tmp(%s), str(%p)\n", tmp, str);
-		}
-	}
+		*str = ft_strjoin(sigle, *ptr_after);
 	else
 		*str = NULL;
 }
@@ -87,8 +66,6 @@ void			epur_str(char **str, int mode)
 	char		*ptr_after;
 
 	ptr = NULL;
-	dprintf(dfd, "*str %s\n", *str);
-	dprintf(dfd, " oy oy%d\n", mode);
 	if (((ptr = ft_strchr(*str, '\\')) != NULL) && (mode == 0))
 	{
 		*ptr = 0;
@@ -98,7 +75,11 @@ void			epur_str(char **str, int mode)
 	else if (((ptr = ft_strchr(*str, '$')) != NULL) && (mode == 0 || mode == 1))
 		epur_variable(str, &ptr);
 	else if (((ptr = ft_strchr(*str, '~')) != NULL) && (mode == 0))
-		epur_home(str, &ptr);
+	{
+		*ptr = 0;
+		ptr_after = ptr + 1;
+		epur_home(str, &ptr_after);
+	}
 }
 
 int				ctrl_tab(char *line, const char **tableau, int itr)
