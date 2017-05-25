02c1fc21570f900871bad423584209d89463f537
Samuel BELINE <sbeline@e1r7p21.42.fr>
2017-05-25 04:37:18 +0200

    last

--- srcs/exec/gestion_redirection.c
+++ srcs/exec/gestion_redirection.c
@@ -6,37 +6,13 @@
 /*   By: sbeline <sbeline@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 01:30:28 by sbeline           #+#    #+#             */
-/*   Updated: 2017/05/25 04:25:12 by sbeline          ###   ########.fr       */
+/*   Updated: 2017/05/25 04:35:36 by sbeline          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 #include "termcaps.h"
 
-int					redirection_fonction(t_node *ast, int *status)
-{
-	t_list_fd		list_fd;
-	int				value;
-
-	if (ast->right_op != NULL)
-		reacast_fd(ast->body->lexem->name_lexem,
-					ast->right_op->body->lexem->name_lexem, &list_fd);
-	else
-		reacast_fd(ast->body->lexem->name_lexem, NULL, &list_fd);
-	if (ast->right_op &&
-		(value = is_dir(ast->right_op->body->lexem->name_lexem)) != FILES)
-	{
-		if (value == REP)
-			ft_putendl_fd("is a directory:", 2);
-		else
-			ft_putendl_fd("cannot redirect", 2);
-	}
-	else if (list_fd.fd[3] == -1)
-		ft_putendl_fd("parse error near `'", 2);
-	else
-		treat_redirection(ast->left_op, list_fd.fd, status);
-	return (0);
-}
 
 void				close_fd_chain(int fd[4])
 {
@@ -59,11 +35,13 @@ void				treat_redirection(t_node *ast, int fd[4], int *status)
 		ft_putendl_fd("fork don't work", 2);
 	if (!pid)
 	{
-		if (fd[2])
+		if (fd[0] && fd[2])
+			dup2(fd[2],fd[0]);
+		else if (fd[2])
 			dup2(fd[2], fd[0]);
-		if (fd[1])
+		else if (fd[1])
 			dup2(fd[2], fd[1]);
-		if (fd[3])
+		else if (fd[3])
 		{
 			dup2(fd[3], 1);
 			dup2(fd[3], 2);
@@ -73,4 +51,30 @@ void				treat_redirection(t_node *ast, int fd[4], int *status)
 	}
 	close_fd_chain(fd);
 	wait(&pid);
+}
+
+int					redirection_fonction(t_node *ast, int *status)
+{
+	t_list_fd		list_fd;
+	int				value;
+
+	if (ast->right_op != NULL)
+		reacast_fd(ast->body->lexem->name_lexem,
+					ast->right_op->body->lexem->name_lexem, &list_fd);
+	else
+		reacast_fd(ast->body->lexem->name_lexem, NULL, &list_fd);
+	printf("%d %d %d %d\n", list_fd.fd[0],list_fd.fd[1],list_fd.fd[2],list_fd.fd[3]);
+	if (ast->right_op &&
+		(value = is_dir(ast->right_op->body->lexem->name_lexem)) != FILES)
+	{
+		if (value == REP)
+			ft_putendl_fd("is a directory:", 2);
+		else
+			ft_putendl_fd("cannot redirect", 2);
+	}
+	else if (list_fd.fd[3] == -1)
+		ft_putendl_fd("parse error near `'", 2);
+	else
+		treat_redirection(ast->left_op, list_fd.fd, status);
+	return (0);
 }
